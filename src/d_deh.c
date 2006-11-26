MagneFire/asteroid-prboom

commit 90e26efa0a347faa39434f15bbb69dbbca6eb24f

Clean up some externs & function prototypes.
git-svn-id: https://svn.prboom.org/repos/trunk/prboom2@1431 df9a1a3e-e9e3-0310-a482-b5a01ba5a74e

src/d_deh.c
@@ -2516,7 +2516,8 @@ void deh_procText(DEHFILE *fpin, FILE* fpout, char *line)
   {
     int c, totlen = 0;
     while (totlen < fromlen + tolen && (c = dehfgetc(fpin)) != EOF)
-      inbuffer[totlen++] = c;
+      if (c != '\r')
+	inbuffer[totlen++] = c;
     inbuffer[totlen]='\0';
   }
 
@@ -2717,7 +2718,7 @@ boolean deh_procStringSub(char *key, char *lookfor, char *newstring, FILE *fpout
   for (i=0;i<deh_numstrlookup;i++)
     {
       found = lookfor ?
-        !stricmp(*deh_strlookup[i].ppstr,lookfor) :
+        !strnicmp(*deh_strlookup[i].ppstr,lookfor,strlen(lookfor)) :
         !stricmp(deh_strlookup[i].lookup,key);
 
       if (found)

src/d_deh.h
@@ -6,7 +6,7 @@
  *  based on BOOM, a modified and improved DOOM engine
  *  Copyright (C) 1999 by
  *  id Software, Chi Hoang, Lee Killough, Jim Flynn, Rand Phares, Ty Halderman
- *  Copyright (C) 1999-2000 by
+ *  Copyright (C) 1999-2006 by
  *  Jess Haas, Nicolas Kalkhof, Colin Phipps, Florian Schulze
  *
  *  This program is free software; you can redistribute it and/or
@@ -39,6 +39,8 @@
 
 #ifndef __D_DEH__
 #define __D_DEH__
+
+void ProcessDehFile(const char *filename, const char *outfilename, int lumpnum);
 
 //
 //      Ty 03/22/98 - note that we are keeping the english versions and

src/d_main.c
@@ -6,7 +6,7 @@
  *  based on BOOM, a modified and improved DOOM engine
  *  Copyright (C) 1999 by
  *  id Software, Chi Hoang, Lee Killough, Jim Flynn, Rand Phares, Ty Halderman
- *  Copyright (C) 1999-2000 by
+ *  Copyright (C) 1999-2006 by
  *  Jess Haas, Nicolas Kalkhof, Colin Phipps, Florian Schulze
  *
  *  This program is free software; you can redistribute it and/or
@@ -83,9 +83,6 @@
 #include "gl_struct.h"
 #endif
 
-// DEHacked support - Ty 03/09/97 // CPhipps - const char*'s
-void ProcessDehFile(const char *filename, const char *outfilename, int lumpnum);
-
 void GetFirstMap(int *ep, int *map); // Ty 08/29/98 - add "-warp x" functionality
 
 // CPhipps - removed wadfiles[] stuff
@@ -119,8 +116,6 @@ FILE    *debugfile;
 int ffmap;
 
 boolean advancedemo;
-
-extern boolean timingdemo, singledemo, demoplayback, fastdemo; // killough
 
 char    wadfile[PATH_MAX+1];       // primary wad file
 char    mapdir[PATH_MAX+1];        // directory of development maps

damerell/prboom-plus

commit 0a93adf48fdbd0dcf8d90c8984b244336319e9a6

fixed(?) cph's bug(?) introduced in r1431
exapmled.deh:
Text 21 23
THE SPIDER MASTERMINDLOL! ITS YOUR MOM! WTF?

During the finale after the end of map30, the monster names (text labels) do not show up correctly 


git-svn-id: https://www.crowproductions.de/repos/prboom/branches/prboom-plus-24/prboom2@2328 df9a1a3e-e9e3-0310-a482-b5a01ba5a74e

src/d_deh.c
@@ -2674,7 +2674,8 @@ boolean deh_procStringSub(char *key, char *lookfor, char *newstring, FILE *fpout
   for (i=0;i<deh_numstrlookup;i++)
     {
       found = lookfor ?
-        !strnicmp(*deh_strlookup[i].ppstr,lookfor,strlen(lookfor)) :
+        //e6y !strnicmp(*deh_strlookup[i].ppstr,lookfor,strlen(lookfor)) :
+        !stricmp(*deh_strlookup[i].ppstr,lookfor) :
         !stricmp(deh_strlookup[i].lookup,key);
 
       if (found)